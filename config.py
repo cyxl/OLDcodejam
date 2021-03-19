483fd3cfd454414ac3b384c537da13dfdea30478
cyxl <cyxlpublic@gmail.com>
2021-03-19 15:29:54 -0600

    aaa

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 import os
 
+load_dotenv(find_dotenv())
 
-env_path = Path('src') / '.env'
-load_dotenv(dotenv_path=env_path)
-
-
-class Config:
-    DEBUG = os.getenv('DEBUG')
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    YT_DEVELOPER_KEY = os.getenv('YT_DEVELOPER_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+YT_DEVELOPER_KEY = os.getenv('YT_DEVELOPER_KEY')
+FIREBASE_URL = os.getenv('FIREBASE_URL')
